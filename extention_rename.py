8508917fb17c53347fd4e6eebc5da009910038f0
Tim <luismuraya11@gmail.com>
2025-02-16 18:06:41 -0500

    - Added getUserInput() function that now takes all user input and calls the rename function - Added return to all functions - main() method calls getUserInput(); allowing the script to be more modular

--- extention_rename.py
+++ extention_rename.py
@@ -1,32 +1,39 @@
 import os
 import os.path
 
-def main() -> None:
-    """Main method of the Program. Prompts the user to enter the file path, extension and the new extention"""
+def getUserInput() -> None:
+    """Prompts the user to enter the file path, extension and the new extention then passes those values to """
     path = input(r"Path: ") + "\\"
     ext = "." + input("Current Ext: ")
     extTo = "." + input("Change to: ")
 
     try:
-        rename(path = path,ext = ext,extTo = extTo)
+        rename(path, ext, extTo)
     except Exception as e:
         print(f"You have a {type(e)}")
         print(e)
 
-    
+    return None
 
 def rename(path, ext, extTo) -> None:
+    """Changes the extension of each file to `extTO` and prints the result."""
     folder = os.listdir(path)
     for file in folder:
 
         if not file.endswith(ext):
-            print (f"{file} => \tNO CHANGE \n")
+            print (f"{file} =>\tNO CHANGE")
             continue
             
         newf = os.path.splitext(file)[0] + extTo
         os.rename(path + file, path + newf)
-        print(f"{file} => \t{newf} \n")
+        print(f"{file} =>\t{newf}")
+
+    return None
     
-    
+def main() -> None:
+    """Main method of the Program"""
+    getUserInput()
+    return None
+
 if __name__ == "__main__":
     main()
